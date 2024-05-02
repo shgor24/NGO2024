662e8ba297e9f4b3da7b84314c9713f111c8626f
shgor24 <168737931+shgor24@users.noreply.github.com>
2024-05-02 14:03:07 +0200

    Add files via upload

---
+++ NGO2024/src/ngo2024/NGO2024.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ngo2024;
+
+import oru.inf.InfDB;
+import oru.inf.InfException;
+/**
+ *
+ * @author sheny
+ */
+public class NGO2024 {
+    
+    private static InfDB idb;
+  
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+          idb = new InfDB("ngo_2024", "3306", "dbAdmin2024", "dbAdmin2024PW");
+          new Inloggning(idb).setVisible(true);
+     }catch (InfException ex){
+      System.out.println(ex.getMessage());
+     }
+    }
+    
+    }
